242c3e08d64093a7216f3dcd79beed5dce68119f
exlineo <contact@exlineo.com>
2023-04-22 14:15:17 +0200

    Base sans les contenus

--- public/js/init.js
+++ public/js/init.js
@@ -1,17 +1,34 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+/*
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDPC7iz8CkV6tTcFAjL4lLOPV1DFQX3LA0",
-  authDomain: "donard-d06fa.firebaseapp.com",
-  projectId: "donard-d06fa",
-  storageBucket: "donard-d06fa.appspot.com",
-  messagingSenderId: "304832651605",
-  appId: "1:304832651605:web:5f7194d4937fad70dc4fdc"
+  apiKey: "AIzaSyAnX69qCnhMTGH-zjPaCpc8MNNtvXO3d54",
+  authDomain: "donard-academie.firebaseapp.com",
+  projectId: "donard-academie",
+  storageBucket: "donard-academie.appspot.com",
+  messagingSenderId: "406132140234",
+  appId: "1:406132140234:web:a5e908448ec9f7da1cec33"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+*/
+
+const lightbox = document.getElementById('lightbox');
+lightbox.addEventListener('click', () => lightbox.classList.remove('affiche'));
+const media = document.querySelector('#lightbox > article');
+
+const setLightbox = (url) => {
+  console.log(url);
+  media.innerHTML = '';
+
+  const figure = document.createElement('figure');
+  figure.style.backgroundImage = `url(${url})`;
+  media.appendChild(figure);
+
+  lightbox.classList.add('affiche');
+}
