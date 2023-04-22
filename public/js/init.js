fbb462596bcba668ccc57a5570aca8445f2ff26d
exlineo <contact@exlineo.com>
2023-04-22 12:18:33 +0200

    Mise en place

---
+++ public/js/init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDPC7iz8CkV6tTcFAjL4lLOPV1DFQX3LA0",
+  authDomain: "donard-d06fa.firebaseapp.com",
+  projectId: "donard-d06fa",
+  storageBucket: "donard-d06fa.appspot.com",
+  messagingSenderId: "304832651605",
+  appId: "1:304832651605:web:5f7194d4937fad70dc4fdc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
